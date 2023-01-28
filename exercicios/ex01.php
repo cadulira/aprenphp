63af6220c649b63920e2af82abd6a90fd116f3dd
cadulira <cadu@tientre.ga>
2023-01-28 14:16:13 -0300

    Adiciona ex III

--- exercicios/ex01.php
+++ exercicios/ex01.php
@@ -1,11 +1,20 @@
 <?php
 
+// ex I
 //$a = 1000;
 //$b = $a / 2;
 
 //echo "O valor de B = " .$b;
 
-$a = 100;
-$b = 3 * $a;
+// ex II
+//$a = 100;
+//$b = 3 * $a;
 
-echo "O valor de B = " .$b;
+//echo "O valor de B = " .$b;
+
+// ex III
+
+$a = 20;
+echo $a;
+echo "<br>";
+echo ++$a;
